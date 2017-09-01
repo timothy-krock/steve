82b00ee5895e1f576829104439dc545642134971
Tim Krock <krocktim@gmail.com>
2017-09-01 01:37:56 -0500

    Add files via upload

---
+++ api/python/check.py
@@ -0,0 +1,15 @@
+#######################################
+## PROGRAM FOR ACCESSING STEVE'S API
+## WRITTEN BY TIM KROCK
+#######################################
+##  https://github.com/roundar/myjson
+
+import myjson
+import json
+
+url = https://api.myjson.com/bins/
+
+
+
+
+
