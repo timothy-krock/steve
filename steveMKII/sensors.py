c7c012fb504386a136ac3e9083896011249fbce9
Tim Krock <krocktim@gmail.com>
2018-01-24 10:15:33 -0600

    Update sensors.py

--- steveMKII/sensors.py
+++ steveMKII/sensors.py
@@ -4,26 +4,40 @@ import json
 import time
 import socket
 import head
+##################################################
+## PROTOTYPE FOR A DATAPACKET
 prototype = {"request": 0, "data": {
                    "type": "sound",
                    "value": 0,
                    "stringVal":" "}}
-
-
+#################################################
+## SORRY THIS IS WIERD. THIS IS GOING TO BE 
+funcPrototype = {
+    "submit": time.sleep(0),
+    "delay": 0, 
+    "response": time.sleep(0)
+}
 ##################################################
 ## THREAD LOOP FOR EACH SENSOR
 ## IT WORKS FOR EACH SENSOR, HOW NEAT IS THAT!
-def run(ip,port,sensor,delay):
+def initIO(sensor,x):
+    ip = head.getIp()
+    port = head.getPort()
     conn = head.connectToHead(ip, port)
     while(1):
-        result = head.sendUp(json.dumps(sensor()),conn)
+        result = head.sendUp(json.dumps(sensor["submit"]()),conn)
 
-        time.sleep(delay)
+        time.sleep(sensor["delay"])
+
+
+
+
+
 
 
 ##################################################
 ## FUNCTION HOME FOR SOUND SENSOR DATA
-def sound():
+def initSound():
     dataPacket = prototype
     dataPacket["request"] = 0
     dataPacket["data"]["type"] = "sound"
@@ -36,9 +50,21 @@ def sound():
         dataPacket["data"]["stringVal"] = "SOUND HEARD"
         dataPacket["data"]["value"] = 0
         return dataPacket
+
+
+
+sound = {
+    "submit": initSound,
+    "delay": .01,
+    "response": time.sleep(0)
+}
+
+
+
+
 ##################################################
 ## FUNCTION HOME FOR LIGHT SENSOR DATA
-def lights():
+def initLights():
     dataPacket = prototype
     dataPacket["request"] = 0
     dataPacket["data"]["type"] = "lights"
@@ -53,9 +79,22 @@ def lights():
         return dataPacket
 
 
+
+lights = {
+    "submit": initLights,
+    "delay": .01,
+    "response": time.sleep(0)
+}
+
+
+
+
+
+
+
 ##################################################
 ## FUNCTION HOME FOR THERMOMETER SENSOR DATA
-def temp():
+def initTemp():
     dataPacket = prototype
     dataPacket["request"] = 0
     dataPacket["data"]["type"] = "temperature"
@@ -68,13 +107,35 @@ def temp():
         dataPacket["data"]["stringVal"] = "COLD"
         return dataPacket
 
+
+temp = {
+    "submit": initTemp,
+    "delay": .01,
+    "response": time.sleep(0)
+}
+
+
+
+
+
 ##################################################
 ## FUNCTION HOME FOR IP ADDRESS MONITOR
-def ip():
+def initIp():
     dataPacket = prototype
     dataPacket["request"] = 0
     dataPacket["data"]["type"] = "ip"
     dataPacket["data"]["value"] = random.random() * 30
     dataPacket["data"]["stringVal"] = "IP"
     return dataPacket
+
+
+
+ip = {
+    "submit": initIp,
+    "delay": .01,
+    "response": time.sleep(0)
+}
+
+
+
 
